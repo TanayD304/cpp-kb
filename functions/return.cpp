25bfd2ac37d6c0148746870ef62e12983239cc74
TanayD304 <tdubey304@gmail.com>
2024-11-06 17:08:05 +0530

    Returns

--- functions/return.cpp
+++ functions/return.cpp
@@ -1,33 +1,43 @@
 /*
 #######################################NOTES#######################################
-
+Return is what function gives as an output
+It can be anywhere in function
+There could be n number of returns buit after 1 gets executed function is terminated
+It is needed to be declared before hand about the return type
+we can return nothing as well - void
 */
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-void sayHello();
-void sayBye();
-void printInt(int);
-void printInt2(int a);
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int condAdd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    else if (b == 0)
+        return a;
+    return a + b;
+}
+
+float circum(int r)
+{
+    return 3.14 * 2 * r;
+}
+
+void sayHello() { cout << "Hello" << endl; }
 
 int main()
 {
     sayHello();
-    return 0;
+    int a, b, r;
+    cin>>a>>b>>r;
+    cout << circum(r) << endl;
+    cout << add(a, b) << endl;
+    cout << condAdd(a, b) << endl;
 }
-
-void sayHello()
-{
-    cout << "Hello" << endl;
-    int a;
-    cin >> a;
-    printInt(a);
-    printInt2(a);
-    sayBye();
-}
-
-void printInt(int a) { cout << a << endl; }
-void printInt2(int a) { cout << a << endl; }
-void sayBye() { cout << "Bye" << endl; }
